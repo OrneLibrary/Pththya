6de6d25c6c910f48180629a0867948a28cab0092
Matrix20085 <mikemaster2000btfu@yahoo.com>
2022-04-05 15:06:12 -0400

    Added logic for VM state

--- olmstead.ps1
+++ olmstead.ps1
@@ -27,7 +27,7 @@ Write-Host "`nSwitches:"
 foreach ($switch in (Get-VirtualSwitch -VMHost $vmHost)) { if ($switch.Name -ne "vSwitch0") { Write-Host $switch.Name }}
 Pause
 Write-Host "Shutting down all VMs"
-foreach ($vm in (Get-VM -Datastore $datastore)) { Stop-VM -VM $vm -Confirm:$false }
+foreach ($vm in (Get-VM -Datastore $datastore)) { if ($vm.PowerState -eq "PoweredOn") { Stop-VM -VM $vm -Confirm:$false } }
 foreach ($vm in (Get-VM -Datastore $datastore)) { Write-Host "Deleting: $vm";Remove-VM -VM $vm -DeletePermanently -Confirm:$false }
 foreach ($nic in (Get-VMHostNetworkAdapter -VMHost $vmHost)){ if (@("vmk1","vmk2").contains($nic.Name)) { Write-Host "Deleting: $nic";Remove-VMHostNetworkAdapter -Nic $nic -Confirm:$false }}
 foreach ($switch in (Get-VirtualSwitch -VMHost $vmHost)) { if ($switch.Name -ne "vSwitch0") {Write-Host "Deleting: $switch";Remove-VirtualSwitch -VirtualSwitch $switch -Confirm:$false }}
