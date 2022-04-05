cd2e0e3d42de6558c5d82b6634c9c476cdac9294
Matrix20085 <mikemaster2000btfu@yahoo.com>
2022-04-05 09:56:35 -0400

    Initial modularization

--- pththya.ps1
+++ pththya.ps1
@@ -10,86 +10,16 @@
     Github: https://github.com/OrneLibrary/Pththya
 #>
 
+Import-Module .\obed.psm1
 
-
-
-function Start-Sleep-Custom($Seconds,$Message) {
-    <#
-        .SYNOPSIS
-        SLeep with message and progress banner
-
-        .PARAMETER Seconds
-        Length of sleep in seconds
-
-        .PARAMETER Message
-        Message to display in progress banner
-
-        .EXAMPLE
-        Start-Sleep-Custom -Seconds 30 -Message "Waiting for thing"
-
-    #>
-
-    $doneDT = (Get-Date).AddSeconds($seconds)
-    while($doneDT -gt (Get-Date)) {
-        $secondsLeft = $doneDT.Subtract((Get-Date)).TotalSeconds
-        $percent = ($Seconds - $secondsLeft) / $Seconds * 100
-        Write-Progress -Activity "Sleeping" -Status $Message -SecondsRemaining $secondsLeft -PercentComplete $percent
-        [System.Threading.Thread]::Sleep(500)
-    }
-    Write-Progress -Activity "Sleeping" -Status $Message -SecondsRemaining 0 -Completed
-}
-
-
-# Check is ISE is being used
-if ($host.name -match 'Windows PowerShell ISE Host')
-{
-  Write-Host "Multiple issues have been noted running this script in ISE.`nPlease use a regular script console to proceed." -ForegroundColor Red -BackgroundColor Black
-  pause
-  exit
-}
-
-
-# Install and import PowerCli
-if (-not (Test-Path -Path $home\Documents\WindowsPowerShell\Modules)) { New-Item -ItemType Directory -Path $home\Documents\WindowsPowerShell\Modules }
-if ((Get-Module -ListAvailable VMware* | Measure-Object | Select-Object -ExpandProperty Count) -ne 73) {
-    Write-Host "Installing VMWare Tools, this may take a while..." -ForegroundColor Cyan -BackgroundColor Black
-    Save-Module -Name VMware.PowerCLI -Path $home\Documents\WindowsPowerShell\Modules
-    Set-PowerCLIConfiguration -Scope AllUsers -ParticipateInCEIP $false -Confirm:$false
-}
-Write-Host "Importing VMWare Tools, this may take a while..." -ForegroundColor Cyan -BackgroundColor Black
-Get-Module -ListAvailable VMware* | Import-Module | Out-Null
-Clear-Host
-
-
-# Get user information and log into vCenter
-while ($true) {
-    $vCenterServer = Read-Host -Prompt "IP or FQDN of vCenter server"
-    $vCenterUsername = Read-Host -Prompt "vCenter username"
-    $vCenterPassword = Read-Host -Prompt "vCenter password" -AsSecureString
-    $vCenterPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($vCenterPassword))
-    Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
-    if (Connect-VIServer -Server $vCenterServer -Protocol https -User $vCenterUsername -Password $vCenterPassword -ErrorAction SilentlyContinue) {
-        Write-Output "Connected to $vCenterServer!"
-        Break
-    } 
-    else { $Error[0] }
-}
-
+Test-PSInterpreter
+Import-PowerCLI
+Initialize-VCenter
+Get-Node
+Get-NodeDatastore
 
 # Password for changing host names on the Kali and Commando boxes
 $guestPassword = Read-Host -Prompt "Password for CPT account on Kali and Commando" -AsSecureString
-
-
-# Get VMHost object
-Write-Host `n`n VM Hosts:`n
-foreach ($hostName in Get-VMHost) { Write-Host $hostName }
-while ($true){
-    $vmHostName = Read-Host -Prompt "Chose VM Host from the selection above"
-    $vmHost = Get-VMHost -Name $vmHostName -ErrorAction SilentlyContinue
-    if (-not $vmHost) { Write-Host "Host name wrong. Try again.`n" -ForegroundColor Red -BackgroundColor Black }
-    else { Break }
-}
-
 
 # Check for correct default NIC configuration
 while ($true){
@@ -101,15 +31,7 @@ while ($true){
 }
 
 
-# Get Datastore object
-Write-Host `n`nData Stores:`n
-foreach ($datastName in ($vmHost | Get-Datastore)) { Write-Host $datastName }
-while ($true) {
-    $datastoreName = Read-Host -Prompt "Chose datastore from the selection above"
-    $datastore = Get-Datastore -Name $datastoreName -ErrorAction SilentlyContinue
-    if (-not $datastore) { Write-Host "Datastore name incorrect!!!" -ForegroundColor Red -BackgroundColor Black }
-    else { Break }
-}
+
 
 # Get operator network information
 $pattern = "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$"
@@ -195,11 +117,11 @@ $currentNIC = Get-NetworkAdapter -VM $currentVM
 Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:21" -Confirm:$false | Out-Null
 New-NetworkAdapter -VM $currentVM -StartConnected -NetworkName "Target" | Out-Null
 Start-VM -VM $currentVM | Out-Null
-Start-Sleep-Custom -Seconds 60 -Message "Waiting for $currentVM to fully boot..."
+Start-SleepCustom -Seconds 60 -Message "Waiting for $currentVM to fully boot..."
 Invoke-VMScript -VM $currentVM -guestUser 'cpt' -guestPassword $guestPassword -ScriptText "sudo nmcli con add con-name TargetNet type ethernet ifname eth0 ipv4.method auto ipv4.ignore-auto-dns false & sudo nmcli con modify 'Wired connection 1' con-name NodeNet ifname eth1 ipv4.method auto ipv4.never-default yes ipv4.dns 172.20.20.1" | Out-Null
 Invoke-VMScript -VM $currentVM -guestUser "cpt" -guestPassword $guestPassword -ScriptText "sudo hostnamectl set-hostname 'CPT-Kali' && sudo sed -i 's/kali/CPT-Kali/g' /etc/hosts && sudo gpasswd --delete cpt kali-trusted" | Out-Null
 Shutdown-VMGuest -VM $currentVM -Confirm:$false | Out-Null
-Start-Sleep-Custom -Seconds 10 -Message "Waiting for $currentVM to fully shutdown..."
+Start-SleepCustom -Seconds 10 -Message "Waiting for $currentVM to fully shutdown..."
 New-Snapshot -VM $currentVM -Name "Gold" -Description "Lab provided Gold image" | Out-Null
 
 
@@ -212,10 +134,10 @@ for ($i=0 ; $i -lt $numOfOperators ; $i++) {
     $currentNIC = Get-NetworkAdapter -VM $currentVM
     Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:$macCounter" -Confirm:$false | Out-Null
     Start-VM -VM $currentVM | Out-Null
-    Start-Sleep-Custom -Seconds 60 -Message "Waiting for $currentVM to fully boot..."
+    Start-SleepCustom -Seconds 60 -Message "Waiting for $currentVM to fully boot..."
     Invoke-VMScript -VM $currentVM -guestUser "cpt" -guestPassword $guestPassword -ScriptText "sudo hostnamectl set-hostname 'Kali-$i' && sudo sed -i 's/kali/Kali-$i/g' /etc/hosts && sudo gpasswd --delete cpt kali-trusted" | Out-Null
     Shutdown-VMGuest -VM $currentVM -Confirm:$false | Out-Null
-    Start-Sleep-Custom -Seconds 10 -Message "Waiting for $currentVM to fully shutdown..."
+    Start-SleepCustom -Seconds 10 -Message "Waiting for $currentVM to fully shutdown..."
     New-Snapshot -VM $currentVM -Name "Gold" -Description "Lab provided Gold image" | Out-Null
     $macCounter++
 }
@@ -230,10 +152,10 @@ $currentNIC = Get-NetworkAdapter -VM $currentVM
 Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:22" -Confirm:$false | Out-Null
 New-NetworkAdapter -VM $currentVM -StartConnected -NetworkName "Target" | Out-Null
 Start-VM -VM $currentVM | Out-Null
-Start-Sleep-Custom -Seconds 60 -Message "Waiting for $currentVM to fully boot..."
+Start-SleepCustom -Seconds 60 -Message "Waiting for $currentVM to fully boot..."
 Invoke-VMScript -VM $currentVM -GuestUser "cpt" -GuestPassword $guestPassword -ScriptText "Rename-Computer -NewName 'CPT-Commando'" | Out-Null
 Shutdown-VMGuest -VM $currentVM -Confirm:$false | Out-Null
-Start-Sleep-Custom -Seconds 10 -Message "Waiting for $currentVM to fully shutdown..."
+Start-SleepCustom -Seconds 10 -Message "Waiting for $currentVM to fully shutdown..."
 New-Snapshot -VM $currentVM -Name "Gold" -Description "Lab provided Gold image" | Out-Null
 
 
@@ -246,10 +168,10 @@ for ($i=0 ; $i -lt $numOfOperators ; $i++) {
     $currentNIC = Get-NetworkAdapter -VM $currentVM
     Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:$macCounter" -Confirm:$false | Out-Null
     Start-VM -VM $currentVM | Out-Null
-    Start-Sleep-Custom -Seconds 60 -Message "Waiting for $currentVM to fully boot..."
+    Start-SleepCustom -Seconds 60 -Message "Waiting for $currentVM to fully boot..."
     Invoke-VMScript -VM $currentVM -GuestUser "cpt" -GuestPassword $guestPassword -ScriptText "Rename-Computer -NewName 'Commando-$i'" | Out-Null
     Shutdown-VMGuest -VM $currentVM -Confirm:$false | Out-Null
-    Start-Sleep-Custom -Seconds 10 -Message "Waiting for $currentVM to fully shutdown..."
+    Start-SleepCustom -Seconds 10 -Message "Waiting for $currentVM to fully shutdown..."
     New-Snapshot -VM $currentVM -Name "Gold" -Description "Lab provided Gold image" | Out-Null
     $macCounter++
 }
